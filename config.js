35378ba7028c531bf353b8d2f188f1bccefd5522
shuiwen898 <93909250+shuiwen898@users.noreply.github.com>
2021-12-28 18:42:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,20 @@
 // 霸都丶傲天 2019/7/9  https://Github.com/AJLoveChina
+<input type="button" value="我是一个按钮" 
+οnclick="javascrtpt:window.location.href='http://blog.sina.com.cn/mleavs'">
+    <script>
+function jump(){
+ window.location.href="http://blog.sina.com.cn/mleavs";
+}
+</script>
+ 
+<input type="button" value="我是一个按钮" οnclick=javascrtpt:jump()>
 var config = {
     // 下面的句子不一定非要7句， 你也可以改成4句，2句话都可以
     lines: [
         "2021年终总结",
        
     ],
+    
      < a  href = "https://www.baidu.com/" >
      < button >点我跳转到度娘！</ button >
 </ a >
@@ -16,5 +26,5 @@ var config = {
     //          2.T不能丢了
     date: "2021-12-29T20:15:00",
     // 双方的名字
-    names: ["笨笨熊"]
+    names: 
 }
