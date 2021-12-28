848958f6d5edc56a32c693879e6297a474d7a4cb
shuiwen898 <93909250+shuiwen898@users.noreply.github.com>
2021-12-28 18:59:02 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,10 @@
 // 霸都丶傲天 2019/7/9  https://Github.com/AJLoveChina
-<input type="button" value="我是一个按钮" 
-οnclick="javascrtpt:window.location.href='http://blog.sina.com.cn/mleavs'">
-    <script>
-function jump(){
- window.location.href="http://blog.sina.com.cn/mleavs";
-}
-</script>
- 
-<input type="button" value="我是一个按钮" οnclick=javascrtpt:jump()>
 var config = {
     // 下面的句子不一定非要7句， 你也可以改成4句，2句话都可以
     lines: [
         "2021年终总结",
        
     ],
-    
-     < a  href = "https://www.baidu.com/" >
-     < button >点我跳转到度娘！</ button >
-</ a >
     // 相爱的时间 记住格式不能写错了, 非常重要
     // 格式一定要和下面这个时间格式一样
     // 分别是 年-月-日T时时:分分:秒秒
@@ -26,5 +13,5 @@ var config = {
     //          2.T不能丢了
     date: "2021-12-29T20:15:00",
     // 双方的名字
-    names: 
+    names: []
 }
