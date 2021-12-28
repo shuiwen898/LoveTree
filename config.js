e975cf7f29033c6a626366cd1d07008414429659
shuiwen898 <93909250+shuiwen898@users.noreply.github.com>
2021-12-28 18:39:09 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,11 @@ var config = {
     // 下面的句子不一定非要7句， 你也可以改成4句，2句话都可以
     lines: [
         "2021年终总结",
-        "< a  href = "https://www.baidu.com/" >
-     < button >点我跳转到度娘！</ button >
-</ a >"
+       
     ],
+     < a  href = "https://www.baidu.com/" >
+     < button >点我跳转到度娘！</ button >
+</ a >
     // 相爱的时间 记住格式不能写错了, 非常重要
     // 格式一定要和下面这个时间格式一样
     // 分别是 年-月-日T时时:分分:秒秒
